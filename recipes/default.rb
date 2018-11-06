10a753452998a04088d3f40de180739761a4e4dc
Elkin Sierra <elkin@sierra.pw>
2018-11-06 12:07:44 -0500

    adds apache recipe to recipes/default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2018, Elkin Sierra, All Rights Reserved.
+
+package 'httpd'
+
+file '/var/www/html/index.html' do
+	content '<h1> Hello World!</h1>'
+end
+
+service 'httpd' do
+	action [:enable, :start]
+end
